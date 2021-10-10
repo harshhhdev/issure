6cb7b985bdc22e70d82d36ba6fab2e7f926454f3
Max Programming <ui.sabuwala@gmail.com>
2021-10-10 13:22:07 +0530

    ✨ Added loader while fetching issue and fixed bugs

---
+++ components/IssueLoader/IssueLoader.tsx
@@ -0,0 +1,11 @@
+import * as S from './IssueLoader.style'
+
+export default function IssueLoader() {
+  return (
+    <S.Balls>
+      <S.FirstBall />
+      <S.SecondBall />
+      <S.ThirdBall />
+    </S.Balls>
+  )
+}
